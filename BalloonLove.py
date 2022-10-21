5f392b8ce64c65b34b9fc6b95d7b83016df24295
Karnagil <108026528+Karnagil@users.noreply.github.com>
2022-10-21 16:55:21 -0500

    Add files via upload

---
+++ BalloonLove.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+x=np.random.randint(200,size=(200))
+y=np.random.randint(200,size=(200))
+colors=np.random.randint(200,size=(200))
+
+dict1={"family":"serif","color":"red","size":10}
+dict2={"family":"serif","color":"crimson","size":10}
+dict3={"family":"font","color":"DarkBlue","size":10}
+
+plt.title("Baloon Love^_^",fontdict=dict1)
+plt.xlabel("@DRESUN999: The Programmer",fontdict=dict2)
+plt.ylabel("@DRESUN999",fontdict=dict3)
+plt.scatter(x,y,c=colors,s=200,cmap="cool",alpha=0.4)
+plt.colorbar()
+plt.show()
