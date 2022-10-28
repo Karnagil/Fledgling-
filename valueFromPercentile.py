66286e47845935a06006bf41690734a788453f36
Karnagil <108026528+Karnagil@users.noreply.github.com>
2022-10-28 11:33:28 -0500

    Update valueFromPercentile.py

--- valueFromPercentile.py
+++ valueFromPercentile.py
@@ -5,14 +5,14 @@ class dresun999():
         self.x=x
         self.y=y
         self.y.sort()
-    def percentile(self):
+    def perval(self):
         a=self.y
         b=int((len(a)/100)*self.x)
         print(a[b-1])
                
 x=[5,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31] 
 
-dresun999(75,x).percentile()
+dresun999(75,x).perval()
 
         
    
