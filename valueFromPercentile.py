389004eb25ae709944e11b6b24dd14bc4c73353c
Karnagil <108026528+Karnagil@users.noreply.github.com>
2022-10-28 19:32:39 -0500

    Add files via upload

---
+++ valueFromPercentile.py
@@ -0,0 +1,17 @@
+#get the value of an array/list via it's percentile:
+class dresun999():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+        self.y.sort()
+    def perval(self):
+        a=self.y
+        b=int((len(a)/100)*self.x)
+        print(a[b])
+               
+x=[5,31,43,48,50,41,7,11,15,39,80,82,32,2,8,6,25,36,27,61,31]
+
+dresun999(75,x).perval()
+
+        
+   
